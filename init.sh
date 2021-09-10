224b8241072fd40da38b0ac1431ab001e620f3d9
Vinicius Rosa <viniciushcrosa@gmail.com>
2021-09-10 14:25:18 -0300

    Atualizaçao 10/09/2021

--- init.sh
+++ init.sh
@@ -1,12 +1,46 @@
+#!/bin/bash
+#Update Distro
 apt-get update
 apt-get upgrade -y
-apt install git curl
+apt dist-upgrade -y
+apt install git curl openvpn -y
 cd ~
+
+#Install nodejs version 14.x
 curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt install nodejs
-npm install -g yarn nodemon
-sudo apt install software-properties-common apt-transport-https wget
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code
+bash nodesource_setup.sh
+apt install nodejs -y
+npm install -g yarn nodemon typescript 
+
+sudo apt install -y software-properties-common apt-transport-https wget
+
+#Install snap packages
+snap install discord
+snap install slack --classic
+
+snap install inkscape
+
+snap install ufw
+
+#code
+snap install sublime-text --classic
+snap install code --classic
+snap install beekeeper-studio
+#code Jetbrains
+snap install phpstorm --classic
+snap install pycharm-professional --classic
+snap install intellij-idea-ultimate --classic
+
+#Docker
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get update
+
+apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io
+docker run hello-world
